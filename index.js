776a67a3617ea3645dc914c243cb07e8fd134950
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-23 19:14:51 +0300

    index.html

--- index.js
+++ index.js
@@ -24,3 +24,21 @@ function renderImageAndDetails(imageDetails){
   
 }
 
+
+
+
+// like button 
+const toggleHeart = () => {
+    const likeButton = document.querySelector("#like-button")
+      
+    likeButton.addEventListener("click", (event)  =>{
+   if(likeButton.textContent === "♡"){
+      likeButton.textContent = "♥"
+   } else {
+      likeButton.textContent = "♡";
+      
+   }
+   })
+  }
+  toggleHeart();
+
