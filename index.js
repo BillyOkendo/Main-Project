c153aeea5917fff449eb59692ab0917143a05492
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-06-25 23:24:37 +0300

    sliding image images

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let imgDescription = document.getElementById('product-des')
 //Getting the image, title and the comments (GET METHOD)
 
 function displayImageAndDetails(){
-    const imageDetails = fetch ("http://localhost:3000/products/1")
+    const imageDetails = fetch ("http://localhost:3000/products/2")
     .then(resp => resp.json())
     .then(data => renderImageAndDetails(data))
     return imageDetails
@@ -42,7 +42,17 @@ const toggleButton = () => {
   }
   toggleButton();
 
+  //list
 
+const findData = () => {
+    const listItem = document.querySelector("#product-list")
+
+    listItem.addEventListener("click",() => {
+        if(listItem = `${product.name}`){
+            
+        }
+    })
+}
 
 
 
@@ -108,3 +118,33 @@ function get(){
 };
 
 get()
+
+
+
+
+
+//sliding samples
+var images = [
+    "https://cdn.vox-cdn.com/thumbor/SJcmPEheS_cbdujd4zbIPTpuXfg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13315959/akrales_181019_3014_0770.jpg",
+    "https://5.imimg.com/data5/HG/XP/MY-29014295/vacuum-cleaner-500x500.jpg",
+    "https://cdn.vox-cdn.com/thumbor/SJcmPEheS_cbdujd4zbIPTpuXfg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/13315959/akrales_181019_3014_0770.jpg",
+    "https://thumbs.dreamstime.com/b/comfortable-couch-orange-red-pillow-spacious-living-room-interior-comfortable-couch-orange-red-pillow-131769657.jpg",
+    "https://m.media-amazon.com/images/I/71czD50UQ5L._AC_UX385_.jpg",
+   "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/mower-1568302588.jpg?crop=1.00xw:0.669xh;0,0.318xh&resize=1200:*",
+    "https://hotpoint.co.ke/media/products/2021/11/VEL32HSVF.png",
+    "https://giftsandflowers.co.ke/wp-content/uploads/2021/04/branded-mugs-in-kenya-2.jpg"
+    
+  ]
+  var imageHead = document.getElementById("product-holder");
+  var i = 0;
+setInterval(function() {
+      imageHead.style.backgroundImage = "url(" + images[i] + ")";
+      i = i + 1;
+      if (i == images.length) {
+        i =  0;
+      }
+}, 2000);
+
+
+
+       
